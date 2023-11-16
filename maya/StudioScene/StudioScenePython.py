18b8f592e34a3d83105f4dff6ae2b313e5a8e50b
Fabian Hagerström <fabianhagerstrom@hotmail.com>
2023-11-16 20:58:09 +0100

    Updated studio scene script
    Switched methods of creating the lights. Imported maya.cmds to edit the lights easier. Changed attributes of stuff.

--- maya/StudioScene/StudioScenePython.py
+++ maya/StudioScene/StudioScenePython.py
@@ -1,34 +1,42 @@
 import pymel.core as pm
+import maya.cmds as cmds
 import mtoa.utils as mutils
 
-def CreateAreaLight(name, intensity, color, position):
-    areaLight = pm.createNode('aiAreaLight', name=name)
+def CreateAreaLight(name, intensity, color, scale, position, rotation):
+    
+    # Create aiAreaLight as a locator (transform node)
+    areaLight = mutils.createLocator("aiAreaLight", asLight=True)
+    
+    # Rename the light
+    areaLight = cmds.rename(name)
 
-    # Get the transform node for the area light
-    transformNode = areaLight.getParent()
+    # Set attributes for the light
+    cmds.setAttr(areaLight + '.intensity', intensity)
+    cmds.setAttr(areaLight + '.color', color[0], color[1], color[2])
+    cmds.setAttr(areaLight + '.normalize', False)
 
-    # Set attributes for the area light
-    areaLight.intensity.set(intensity)
-    areaLight.color.set(color)
-    transformNode.translate.set(position)
+    # Set transform attributes
+    cmds.scale(scale[0], scale[1], scale[2], name)
+    cmds.move(position[0], position[1], position[2], name)
+    cmds.rotate(rotation[0], rotation[1], rotation[2], name)
 
 
 def CreateStudioScene():
     # Create a cube
-    cube = pm.polyCube()[0]
+    cube = pm.polyCube(name="modelPlaceholder", width=5, height=5, depth=5)[0]
 
-    leftLight = CreateAreaLight('leftLight', 2.0, (1, 1, 1), (-5, 5, 0))
-    rightLight = CreateAreaLight('rightLight', 2.0, (1, 1, 1), (5, 5, 0))
-    roofLamp = CreateAreaLight('roofLamp', 2.0, (1, 1, 1), (0, 10, 0))
+    leftLight = CreateAreaLight('leftLight', 2.0, (1, 1, 1), (5, 5, 5), (-9, 6, 0), (0, -90, 0))
+    rightLight = CreateAreaLight('rightLight', 2.0, (1, 1, 1), (5, 5, 5), (9, 6, 0), (0, 90, 0))
+    roofLamp = CreateAreaLight('roofLamp', 2.0, (1, 1, 1), (5, 5, 5), (0, 10, 0), (-90, 0, 0))
 
     # Create a plane
-    backdropPlane = pm.polyPlane(name='backdrop', width=20, height=15, subdivisionsX=1, subdivisionsY=1)[0]
+    backdropPlane = pm.polyPlane(name='backdrop', width=30, height=15, subdivisionsX=1, subdivisionsY=1)[0]
     pm.rotate(backdropPlane, 90, 0, 0)
     pm.move(backdropPlane, 0, 7, -9)
     
     backdropPlane = pm.ls(selection=True)[0]
     pm.select(backdropPlane.e[0])
-    pm.polyExtrudeEdge(ltz = 15.0)
+    pm.polyExtrudeEdge(ltz = 30.0)
     pm.select(backdropPlane.e[0])
     pm.polyBevel(sg = 6, o = 1.4)
     
@@ -36,16 +44,13 @@ def CreateStudioScene():
     pm.delete()
     pm.select(backdropPlane.f[-1])
     pm.delete()
-
-    # Create an Arnold skydome light for environment lighting
-    environmentLight = pm.createNode('aiSkyDomeLight', name='environmentLight')
-
-    # Set attributes for skydome light
-    environmentLight.color.set(1, 1, 1)
-    environmentLight.intensity.set(0.5)
-
-    # Set render settings to use Arnold
-    pm.setAttr('defaultRenderGlobals.currentRenderer', 'arnold', type='string')
+    
+    # Create SkyDome light
+    mutils.createLocator("aiSkyDomeLight", asLight=True)
+    
+    skyLight = cmds.rename('skyLight')
+    pm.setAttr(skyLight + '.intensity', 0.25)
+    pm.setAttr(skyLight + ".aiNormalize", False)
 
     print("Studio scene created successfully!")
 
